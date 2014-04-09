cf54328e9bf322b55bdda3e1ab73e0a3ca7aa835
itamar c <itc404@gmail.com>
2014-04-09 22:54:20 +0300

    intergrating facebook

---
+++ www/js/facebook.js
@@ -0,0 +1,40 @@
+
+//These are the notifications that are displayed to the user through pop-ups if the above JS files does not exist in the same directory-->
+
+if ((typeof cordova == 'undefined') && (typeof Cordova == 'undefined')) alert('Cordova variable does not exist. Check that you have included cordova.js correctly');
+if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+FB.Event.subscribe('auth.login', function(response) {
+                   alert('auth.login event');
+                   });
+
+FB.Event.subscribe('auth.logout', function(response) {
+                   alert('auth.logout event');
+                   });
+
+
+var facebook = {
+      init:function() {
+            try {
+                  FB.init({ appId: "381782658606958", nativeInterface: CDV.FB, useCachedDialogs: false });
+            } catch (e) {
+                  alert(e);
+            }
+      }
+
+
+      login: function() {
+          FB.login(
+                   function(response) {
+                   if (response.session) {
+                   alert('logged in');
+                   } else {
+                   alert('not logged in');
+                   }
+                   },
+                   { scope: "email" }
+                   );
+      }
+}
+            
--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        facebook.init();
     },
     // Bind Event Listeners
     //
