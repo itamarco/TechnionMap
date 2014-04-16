b78ad0f29579f0975c29b882f719d60ce587f205
itamar c <itc404@gmail.com>
2014-04-16 20:16:57 +0300

    arcGis basic

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -49,3 +32,42 @@ var app = {
     }
 };
 
+var geoLocation = {
+    get : function() {
+        log.info("fetching geolocation");
+        navigator.geolocation.getCurrentPosition(this.onSuccsess, this.onError);
+    },
+    onSuccsess : function(pos) {
+        /*
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + pos.coords.latitude              + '<br />' +
+                            'Longitude: '          + pos.coords.longitude             + '<br />' +
+                            'Altitude: '           + pos.coords.altitude              + '<br />' +
+                            'Accuracy: '           + pos.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + pos.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + pos.coords.heading               + '<br />' +
+                            'Speed: '              + pos.coords.speed                 + '<br />' +
+                            'Timestamp: '          +                                   pos.timestamp          + '<br />';
+                            
+        */              
+        //data with map 
+            var text = "<div>Latitude: " + pos.coords.latitude + 
+                        "<br/>" + "Longitude: " + pos.coords.longitude + "<br/>" + 
+                        "Accuracy: " + pos.coords.accuracy + "m<br/>" + "</div>";
+            $("#cur_position").html(text);
+            console.log(text);
+            
+            var mapwidth = $(window).width();//parseInt($('#map').css("width"), 10);  // remove 'px' from width value
+            var mapheight = 180;//parseInt($('#map').css("height"), 10);
+            //$('img#map').css('visibility','visible');
+            $('img#map').attr('src', "http://maps.googleapis.com/maps/api/staticmap?center=" + 
+                pos.coords.latitude + "," + pos.coords.longitude + 
+                "&zoom=13&size=" + mapwidth + "x" + mapheight + "&maptype=roadmap&markers=color:green%7C" +
+                pos.coords.latitude + "," + pos.coords.longitude + "&sensor=false");
+
+    },
+    onError : function(error) {
+        $("#cur_position").html('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+};
