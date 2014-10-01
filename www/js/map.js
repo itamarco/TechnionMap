09e20016ef3f9863a6a088889a3bcf30aabb4e89
itamar c <itc404@gmail.com>
2014-10-01 22:40:26 +0300

    Revert "del2"
    This reverts commit e84a3301b5d022b1060c6d9bcef6aee8cf2551db.

--- www/js/map.js
+++ www/js/map.js
@@ -8,7 +8,13 @@
 
 			function initGmap(){
 
-
+				var mapsEngineLayer = new google.maps.visualization.MapsEngineLayer({
+				  mapId: '08097405334273999136-05414812628978701557',
+				  layerKey: 'bldgs',
+				  map: map,
+				  clickable: true,
+				  suppressInfoWindows: false
+				});
 
 
 				// map = new GMaps({
@@ -76,14 +82,6 @@
 
 
 /////////////////////////////// YAKIR ////////////////////////////
-				// var mapsEngineLayer = new google.maps.visualization.MapsEngineLayer({
-				//   mapId: '08097405334273999136-05414812628978701557',
-				//   layerKey: 'bldgs',
-				//   map: map,
-				//   clickable: true,
-				//   suppressInfoWindows: false
-				// });
-
 
 				// var mapOptions = {
 				//   center: new google.maps.LatLng(32.777522, 35.023138),
@@ -122,20 +120,45 @@
 
 
 
-			google.maps.Map.prototype.addPerson = function(user){
+			Map.prototype.addPerson = function(user){
 				log.info("UUUUUUUUUUUU");
-
-
+				if(!user.isValid()){
+					return;
+				}
+				this.addMarker({
+					lat: user.loc.lat,
+					lng: user.loc.lng,
+					title: 'Lima',
+					icon: {
+						url: user.pic,
+						scaledSize: {
+							width: 50,
+							height: 50
+						}
+					},
+					//shadow: 'schools_maps.shadow.png',
+					details: {
+						database_id: 42,
+						author: 'HPNeo'
+					},
+					click: function() {
+						this.markerClick(user);
+					}
+				});
 			}
 
-			google.maps.Map.prototype.updateMarkers = function(){
+			Map.prototype.updateMarkers = function(){
 				log.info("UUUUUUUUUUUU");
+				this.removeMarkers();
 
-
+				this.addPerson(me);
+				for(var i in activeFriends){
+					map.addPerson(activeFriends[i]);
+				}
 			};
 
 
-			google.maps.Map.prototype.markerClick = function(user){
+			Map.prototype.markerClick = function(user){
 				log.info(user.name);
 			}
 
